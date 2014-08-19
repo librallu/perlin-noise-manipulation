ecbbb5059cdedf53b201d1d1a005a7fd5bbe54d8
Luc LIBRALESSO <libralesso.l@gmail.com>
2014-08-19 17:52:54 +0200

    ui modifications

--- ui.R
+++ ui.R
@@ -10,13 +10,27 @@ shinyUI(pageWithSidebar(
   ),
   mainPanel(
     HTML("
-         <p>Hi ! The algorithm used in this application is called the Perlin Noise, it was designed by <strong>Ken Perlin</strong>.
+         <p>Hi ! The algorithm used in this application is called the Perlin Noise, it was designed by 
+            <strong>Ken Perlin</strong>.
          He designed it for making better textures for film Tron.</p>
          <p>With this application, you can create your own textures or world. Please move some parameters
       to see how the image behaves.</p>
+
+      <p>User can change :
       <ul>
-        <li>source : <a href=\"http://webstaff.itn.liu.se/~stegu/TNM022-2005/perlinnoiselinks/perlin-noise-math-faq.html\" alt=\"webstaff.itn.liu.se\">webstaff.itn.liu.se</a></li>
-        <li>application sources on github : <a href=\"https://github.com/librallu/perlin-noise-manipulation.git\" alt=\"github\">github</a></li>
+        <li><strong>noise impact :</strong> adds noise to the output</li>
+        <li><strong>subspace length :</strong> Choose the peak frequency</li>
+        <li><strong>color theme :</strong> [Black and White, Sky, Fire] Change output color theme</li>
+        <li><strong>interpolation method :</strong> [linear, sinusoidal, quadratic] linear and sinusoidal are 
+          simplier but quadratic method has better rendering.</li>
+      </ul>
+
+
+      <ul>
+        <li>source : <a href=\"http://webstaff.itn.liu.se/~stegu/TNM022-2005/perlinnoiselinks/perlin-noise-math-faq.html\" 
+          alt=\"webstaff.itn.liu.se\">webstaff.itn.liu.se</a></li>
+        <li>application sources on github : <a href=\"https://github.com/librallu/perlin-noise-manipulation.git\" 
+          alt=\"github\">github</a></li>
       </ul>
     "),
     imageOutput("image", height = 300, width=300)
